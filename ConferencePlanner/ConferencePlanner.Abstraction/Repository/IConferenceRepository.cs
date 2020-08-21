5513a4ec50352f79c2cf4e122ce46096a603125c
Andrei Stancescu <andrei.stancescu@totalsoft.ro>
2020-08-21 16:06:50 +0300

    Modificare V3 (Ruleaza)

--- ConferencePlanner/ConferencePlanner.Abstraction/Repository/IConferenceRepository.cs
+++ ConferencePlanner/ConferencePlanner.Abstraction/Repository/IConferenceRepository.cs
@@ -12,3 +12,15 @@ namespace ConferencePlanner.Abstraction.Repository
         
     }
 }
+using ConferencePlanner.Abstraction.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConferencePlanner.Abstraction.Repository
+{
+    public interface IConferenceRepository
+    {
+        List<ConferenceModel> GetConference(string name);
+    }
+}
