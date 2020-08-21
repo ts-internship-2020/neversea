eac6951abe2ed76ef2dc628eb76b34737059ac6a
Adela Brezuica <adela.brezuica@TS1370>
2020-08-21 14:57:52 +0300

    adaugare serviciu in Program.cs, modificare select baza de date, hihi

--- ConferencePlanner/ConferencePlanner.Abstraction/Repository/IConferenceRepository.cs
+++ ConferencePlanner/ConferencePlanner.Abstraction/Repository/IConferenceRepository.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 namespace ConferencePlanner.Abstraction.Repository
 {
-    public interface IGetConferenceRepository
+    public interface IConferenceRepository
     {
         List<ConferenceModel> GetConference(string name);
     }
--- ConferencePlanner/ConferencePlanner.WinUi/MainPage.cs
+++ ConferencePlanner/ConferencePlanner.WinUi/MainPage.cs
@@ -15,15 +15,14 @@ namespace ConferencePlanner.WinUi
     public partial class MainPage : Form
 
     {
-        private readonly IGetConferenceRepository _getConferenceRepository;
+        private readonly IConferenceRepository _getConferenceRepository;
         public List<ConferenceModel> Conferences { get; set; }
     
-        public GetConferenceRepository GetConferenceRepository { get; set; }
 
-        public MainPage(IGetConferenceRepository getConferenceRepository, GetConferenceRepository gcf, List<ConferenceModel> listuta)
+        public MainPage(IConferenceRepository getConferenceRepository/*, GetConferenceRepository gcf, List<ConferenceModel> listuta*/)
         {
-            GetConferenceRepository = gcf;
-            Conferences = listuta;
+            /*GetConferenceRepository = gcf;
+            Conferences = listuta;*/
             _getConferenceRepository = getConferenceRepository;
 
             InitializeComponent();
@@ -34,7 +33,7 @@ namespace ConferencePlanner.WinUi
         private void MainPage_Load(object sender, EventArgs e)
         {
             //  var conferences = this.Conferences;
-            dgvConferences.DataSource = GetConferenceRepository.GetConference("");
+            dgvConferences.DataSource = _getConferenceRepository.GetConference("");
         }
     }
 }
