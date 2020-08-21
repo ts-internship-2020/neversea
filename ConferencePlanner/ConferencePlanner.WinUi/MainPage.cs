02054d110003ef06862871ddec1df80012be5fa5
Adela Brezuica <adela.brezuica@TS1370>
2020-08-21 15:02:56 +0300

    reparare buguri MainForm - load, label, listbox

--- ConferencePlanner/ConferencePlanner.WinUi/MainPage.cs
+++ ConferencePlanner/ConferencePlanner.WinUi/MainPage.cs
@@ -36,10 +36,6 @@ namespace ConferencePlanner.WinUi
             dgvConferences.DataSource = _getConferenceRepository.GetConference("");
         }
 
-        private void MainPage_Load(object sender, EventArgs e)
-        {
-
-        }
 
         private void label1_Click(object sender, EventArgs e)
         {
