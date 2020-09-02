5e6901f10b3a6cc91ff73fabd447355f9b4e3cb5
daniel.tucmeanu <daniel.tucmeanu@totalsoft.ro>
2020-09-02 09:19:38 +0300

    comit pt pull

--- ConferencePlanner/ConferencePlanner.RepositoryEf/Repository/GetDemoRepository.cs
+++ ConferencePlanner/ConferencePlanner.RepositoryEf/Repository/GetDemoRepository.cs
@@ -1,28 +1,30 @@
 ﻿using ConferencePlanner.Abstraction.Model;
 using ConferencePlanner.Abstraction.Repository;
 using ConferencePlanner.Repository.Ef.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks.Dataflow;
 
 namespace ConferencePlanner.Repository.Ef.Repository
 {
     public class GetDemoRepository : IGetDemoRepository
     {
-        private readonly neverseaContext _dbContext;
+        private readonly neverseaContext _neverseaContext;
 
         public GetDemoRepository(neverseaContext dbContext)
         {
-            _dbContext = dbContext;
+            _neverseaContext = dbContext;
         }
 
         public List<DemoModel> GetDemo(string name)
         {
-            List<Demo> demos = _dbContext.Demo.ToList();
-
+            List<Demo> demos = _neverseaContext.Demo.ToList();
+            List<Conference> conferences = _neverseaContext.Conference.Include(x => x.ConferenceXspeaker).ToList(); //join Include
             List<DemoModel> demoModels = demos.Select(a => new DemoModel() { Id = a.Id, Name = a.Name }).ToList();
-
+            Conference conference = _neverseaContext.Conference.FirstOrDefault();
             return demoModels;
         }
     }
--- ConferencePlanner/ConferencePlanner.RepositoryEf/readme.txt
+++ ConferencePlanner/ConferencePlanner.RepositoryEf/readme.txt
@@ -1,3 +1,4 @@
 ﻿View->Terminal
 dotnet restore
+dotnet tool install --global dotnet-ef
 dotnet ef dbcontext scaffold "Server=ts-internship-2019.database.windows.net;Database=neversea;User Id=usr2020;Password=n39fn0n2_j32-(#;MultipleActiveResultSets=true;" Microsoft.EntityFrameworkCore.SqlServer -o Entities -f --no-build
